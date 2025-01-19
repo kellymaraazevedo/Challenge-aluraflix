41442ba4fc5ff82ee035caeae562d8d535369e55
kellymaraazevedo <kellymaraazevedo@gmail.com>
2025-01-19 17:55:40 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-
 import React, { useState } from 'react';
 import './App.css';
 
 import VideoList from './components/VideoList';
 import VideoForm from './components/VideoForm';
-import Footer from './components/Footer';  // Importando o Footer
+import Footer from './components/Footer';
 
 function App() {
   const [videos, setVideos] = useState([
-    { id: 1, title: 'Vídeo 1', description: 'Descrição do vídeo 1' },
-    { id: 2, title: 'Vídeo 2', description: 'Descrição do vídeo 2' },
+    { id: 1, title: 'Vídeo 1', description: 'Criação de um formulário', url: 'https://youtu.be/m4ORMy802r8' },
+    { id: 2, title: 'Vídeo 2', description: 'Front-End para iniciantes', url: 'https://youtu.be/I6xTDxkjjqk' },
+    { id: 3, title: 'Vídeo 3', description: 'O que faz uma desenvolvedora front-end?', url: 'https://youtu.be/ZY3-MFxVdEw' },
   ]);
   const [videoToEdit, setVideoToEdit] = useState(null);
 
@@ -46,7 +46,7 @@ function App() {
         onEdit={handleEditVideo}
         onDelete={handleDeleteVideo}
       />
-      <Footer /> {/* Adicionando o Footer aqui */}
+      <Footer />
     </div>
   );
 }
